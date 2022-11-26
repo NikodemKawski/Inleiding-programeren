58042b348a0a91b289b0dd38074c271b5a9db557
NikodemKawski <r0957600@student.thomasmore.be>
2022-11-26 14:20:11 +0100

    .

---
+++ 07/07.01/07.01/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+namespace _07._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> getallen = new List<int>();
+            string input = "";
+            bool test;
+            int number;
+            int aantalPositief = 0;
+            int negatief = 0;
+
+            do
+            {
+                Console.WriteLine("Geef een cijfer");
+                input = Console.ReadLine();
+                test = int.TryParse(input, out number);
+                getallen.Add(number);
+            } while (!String.IsNullOrWhiteSpace(input));
+
+            for (int i = 0; i < getallen.Count; i++)
+            {
+                if (getallen[i] < 0)
+                {
+                    negatief += getallen[i];
+                }
+                else if (getallen[i] > 0)
+                {
+                    aantalPositief++;
+                }
+            }
+            Console.WriteLine($"Aantal positief: {aantalPositief}");
+            Console.WriteLine($"Negatieve som: {negatief}");
+        }
+    }
+}
+
---
+++ 07/07.03/07.03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> getallen = new List<int>();
+
+        string invoer = " ";
+        int getal, hoogste, laagste;
+        bool test;
+        string output = " ";
+
+        do
+        {
+            //Console.WriteLine("Geef een cijfer: ");
+            invoer = Console.ReadLine();
+            test = int.TryParse(invoer, out getal);
+            getallen.Add(getal);
+        } while (!String.IsNullOrWhiteSpace(invoer));
+
+        getallen.Sort();
+
+        hoogste = getallen.First();
+        laagste = getallen.Last();
+
+        output = $"Laagste getal: {hoogste}\nHoogste getal: {laagste}";
+
+        Console.WriteLine(output);
+    }
+}
